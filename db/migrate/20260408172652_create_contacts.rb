b1f756267d8ab03a19e745217502c05a4acfb45e
Larry Reid <lcreid@jadesystems.ca>
2026-04-08 17:30:24 +0000

    Generate contact and migrate

---
+++ db/migrate/20260408172652_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email_address
+      t.string :message
+
+      t.timestamps
+    end
+  end
+end
