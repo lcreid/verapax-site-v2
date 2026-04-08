c4a4fdcfb9b576c3c5ceea4d44a00066d45ca5a6
Larry Reid <lcreid@jadesystems.ca>
2026-04-08 14:20:12 +0000

    Contact us old way

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   get "verapax-means", to: "home#verapax_means"
+  get "contact-us", to: "home#contact_us"
 end
