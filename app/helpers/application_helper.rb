bdbb80ab2792435d4849da58f587dda2967427be
Larry Reid <lcreid@jadesystems.ca>
2026-04-08 19:12:46 +0000

    Hero helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def hero(&)
+    tag.div(class: "hero mb-5") do
+      tag.div(class: "d-flex align-items-center h-100") do
+        tag.div(class: "hero-image h-100 w-100 d-flex align-items-end") do
+          tag.div(class: "hero-content pb-5") do
+            capture(&) if block_given?
+          end
+        end
+      end
+    end
+  end
 end
