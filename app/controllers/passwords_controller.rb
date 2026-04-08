040d679ddd7680e20430f7cb5a96ca18d746724a
Larry Reid <lcreid@jadesystems.ca>
2026-04-08 21:19:34 +0000

    Format password forms

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,6 +4,7 @@ class PasswordsController < ApplicationController
   allow_unauthenticated_access
   before_action :set_user_by_token, only: %i[ edit update ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_password_path, alert: "Try again later." }
+  layout "sessions_and_registrations"
 
   def new
   end
