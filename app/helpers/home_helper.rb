57b006f4d6b13a449fa7c517db20dd7dc3e6a0f1
Larry Reid <lcreid@jadesystems.ca>
2026-04-09 21:44:06 +0000

    Who we work with

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def partner_link(href:, src:, alt:, &)
+    link_to(href:, class: "d-block text-center my-auto") do
+      image = image_tag(src, class: "img-fluid client-logo d-block m-auto px-5", alt:) if src.present?
+      safe_join([ image, capture(&) ].compact)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   get "verapax-means", to: "home#verapax_means"
+  get "who-we-work-with", to: "home#who_we_work_with"
 
   resources :contacts, only: [ :create, :new, :show ]
   get "contacts/thank-you", to: "contacts#thank_you"
