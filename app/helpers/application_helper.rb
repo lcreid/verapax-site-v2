c524c2c000fb30f2a6d0103ef21a3609b9d8ce01
Larry Reid <lcreid@jadesystems.ca>
2026-04-09 02:51:01 +0000

    Some links for ergonomics and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,32 @@ module ApplicationHelper
       end
     end
   end
+
+  def link_to_home
+    link_to(root_path) do
+      tag.i(class: "bi bi-house")
+    end
+  end
+
+  def sign_in_link
+    return if Current.user.present?
+
+    link_to(root_path, class: "btn") do
+      tag.i(class: "bi bi-box-arrow-right") + tag.span("Sign In", class: "d-none d-md-inline ps-md-2")
+    end
+  end
+
+  def sign_out_link
+    return unless Current.user.present?
+
+    button_to(session_path, method: :delete, form_class: "ms-auto", class: "btn") do
+      tag.i(class: "bi bi-box-arrow-left") + tag.span("Sign Out", class: "d-none d-md-inline ps-md-2")
+    end
+  end
+
+  def top_menu
+    tag.div(class: "d-flex") do
+      safe_join([ sign_in_link, sign_out_link ].compact)
+    end
+  end
 end
