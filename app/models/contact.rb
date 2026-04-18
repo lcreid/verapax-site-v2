8c8969646c9934b69a0f053931ce88fb778157c3
Larry Reid <lcreid@jadesystems.ca>
2026-04-18 10:07:18 -0600

    Revert "Add previous to Contact"
    This reverts commit b3c5d45fad0d3d693066d2fd939eaf597fe2b9d7.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
-  belongs_to :previous, class_name: "Contact", optional: true
-  has_many :responses, class_name: "Contact", foreign_key: :previous_id
 end
--- db/migrate/20260417220822_add_previous_reference_to_contact.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddPreviousReferenceToContact < ActiveRecord::Migration[8.1]
-  def change
-    add_reference :contacts, :previous, null: true, foreign_key: { to_table: :contacts }
-  end
-end
