47dfe97dd8ff2110fe4560b8eb58c7c2e87bec4e
Larry Reid <lcreid@jadesystems.ca>
2026-04-20 00:24:54 +0000

    First case of a working Recaptcha Much to do still.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,13 +10,16 @@ class HomeController < ApplicationController
   end
 
   def create_contact
+    success = verify_recaptcha(action: "send_contact_us", minimum_score: 0.5, secret_key: ENV["RECAPTCHA_SECRET_KEY"])
+
     @contact = Contact.build(
       params.require(:contact).permit(:name, :email_address, :message, :start_of_thread_id),
     )
 
-    if @contact.valid?(:unauthenticated) && @contact.save!
+    if success && @contact.valid?(:unauthenticated) && @contact.save!
       redirect_to contact_thank_you_path
     else
+      Rails.logger.debug("rendering contact_us unprocessable_entity success: #{success}")
       render "contact_us", locals: { email_help:, start_of_thread: nil }, status: :unprocessable_entity
     end
   end
