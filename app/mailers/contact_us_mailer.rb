73271358f836b861056804b043edbc99b15ad0b4
Larry Reid <lcreid@jadesystems.ca>
2026-05-06 18:25:07 +0000

    Send to ingo@verapax.org

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -3,12 +3,11 @@
 class ContactUsMailer < ApplicationMailer
   def forward_contact_us
     @contact = params[:contact]
-    recipients = User.where(receives_contacts: true)
-    raise ArgumentError, "No recipients for Contact Us messages." if recipients.empty?
+    recipient = ENV.fetch("CONTACT_US_EMAIL_ADDRESS", "info@verapax.org")
 
     headers({
       reply_to: email_address_with_name(@contact.email_address, @contact.name),
     })
-    mail(to: recipients.map(&:email_address), subject: "Message from Contact Us")
+    mail(to: recipient, subject: "Message from Contact Us")
   end
 end
